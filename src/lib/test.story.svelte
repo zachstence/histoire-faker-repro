802f3a53973f835eb0bb90701e017f2d92c4d720
Zachary Stence <zachstence@gmail.com>
2023-12-29 19:49:08 -0600

    Initial commit

---
+++ src/lib/test.story.svelte
@@ -0,0 +1,13 @@
+<script>
+    import { faker } from '@faker-js/faker'
+
+    export let Hst;
+
+    let someBoolean = false
+</script>
+
+<Hst.Story>
+    <svelte:fragment slot="controls">
+        <Hst.Checkbox title="someBoolean" bind:value={someBoolean} />
+    </svelte:fragment>
+</Hst.Story>
